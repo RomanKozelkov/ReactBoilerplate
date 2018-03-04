8b0c0f9b4cc5c121bf2ddd9d3f7dc60e5f0d6b29
Roman Kozelkov <kozelkov.roman@gmail.com>
2018-03-04 13:15:46 +0300

    add header demo

--- src/core/App.tsx
+++ src/core/App.tsx
@@ -3,16 +3,54 @@ import {Provider} from 'react-redux';
 import {store, history} from "../redux/store";
 import {Route} from 'react-router';
 import {ConnectedRouter} from 'react-router-redux';
+import Container from "reactstrap/lib/Container";
+import Row from "reactstrap/lib/Row";
+import Navbar from "reactstrap/lib/Navbar";
+import NavbarBrand from "reactstrap/lib/NavbarBrand";
+import NavbarToggler from "reactstrap/lib/NavbarToggler";
+import Collapse from "reactstrap/lib/Collapse";
+import Nav from "reactstrap/lib/Nav";
+import NavItem from "reactstrap/lib/NavItem";
+import NavLink from "reactstrap/lib/NavLink";
+import {UncontrolledDropdown} from "reactstrap";
+import DropdownToggle from "reactstrap/lib/DropdownToggle";
+import DropdownMenu from "reactstrap/lib/DropdownMenu";
+import DropdownItem from "reactstrap/lib/DropdownItem";
 require('../../scss/_custom.scss');
 
 interface MainPageProps {
 }
 
 export class MainPage extends React.Component<MainPageProps> {
+
+	toggle() {
+
+	}
+
 	render() {
 		return (
 			<div>
-				Hello, world!
+				<Navbar color="inverse" dark expand='md'>
+					<NavbarBrand href="/">React boilerplate</NavbarBrand>
+					<NavbarToggler onClick={this.toggle} />
+					<Collapse isOpen={false} navbar>
+						<Nav className="ml-auto" navbar>
+							<NavItem>
+								<NavLink href="#">Demo 1</NavLink>
+							</NavItem>
+							<NavItem>
+								<NavLink href="#">Demo 2</NavLink>
+							</NavItem>
+						</Nav>
+					</Collapse>
+				</Navbar>
+				<div className='container'>
+					<Container>
+						<Row>
+							Hello, world!
+						</Row>
+					</Container>
+				</div>
 			</div>
 		);
 	}
